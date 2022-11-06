68f4d0b6fae8e54940872da6825b80f63b32918f
KonstantinShmondrik <shmond@mail.ru>
2022-11-06 23:59:14 +0600

    структура проекта

---
+++ AppleMapsTracker_KS/AppleMapsTracker_KS/App/Flows/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMapsTracker_KS
+//
+//  Created by Константин Шмондрик on 06.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
